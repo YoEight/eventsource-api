2ed8e7d6533748028053c65f79937a90481bb672
YoEight <yo.eight@gmail.com>
2016-12-18 22:31:01 +0100

    First commit

---
+++ library/EventSource.hs
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module : EventSource
+-- Copyright : (C) 2016 Yorick Laupa
+-- License : (see the file LICENSE)
+--
+-- Maintainer : Yorick Laupa <yo.eight@gmail.com>
+-- Stability : provisional
+-- Portability : non-portable
+--
+--------------------------------------------------------------------------------
+module EventSource where
+
+--------------------------------------------------------------------------------
+import EventSource.Types
---
+++ library/EventSource/Types.hs
@@ -0,0 +1,14 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module : EventSource.Types
+-- Copyright : (C) 2016 Yorick Laupa
+-- License : (see the file LICENSE)
+--
+-- Maintainer : Yorick Laupa <yo.eight@gmail.com>
+-- Stability : provisional
+-- Portability : non-portable
+--
+--------------------------------------------------------------------------------
+module EventSource.Types where
+
+--------------------------------------------------------------------------------
